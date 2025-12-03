5c56b618ca1c514961d4288739f5af7e903d2a73
Aggelos1991 <aggelosmc@gmail.com>
2025-12-04 00:29:25 +0200

    Update index.js

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import openaiRouter from "./openai.js";
+import storyRouter from "./story.js";
 
 const router = express.Router();
 
+// /api/openai
 router.use("/openai", openaiRouter);
+
+// /api/story
+router.use("/story", storyRouter);
 
 export default router;
